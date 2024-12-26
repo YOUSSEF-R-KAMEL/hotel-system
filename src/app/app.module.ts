9e9b98df0df51430be7dc8e2babd07dc7b6df306
Youssef Raafat <youssefrafat700@gmail.com>
2024-12-26 11:36:41 +0200

    [feat]: add toastr, enum routes, global interceptor, shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
     provideClientHydration(),
---
+++ src/app/features/auth/routes/enum.ts
@@ -0,0 +1,6 @@
+export enum authRoutes {
+  login= "/auth/login",
+  register= "/auth/register",
+  requestResetPassword= "/auth/request-reset-password",
+  resetPassword= "/auth/reset-password",
+}
