4dd37042adfabd51a7af749ce6cecc7925ad9536
Youssef Raafat <youssefrafat700@gmail.com>
2024-12-26 22:35:43 +0200

    [feat]: add register component and auth services

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ImageCropperComponent } from 'ngx-image-cropper';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    ImageCropperComponent
   ]
 })
 export class AuthModule { }
