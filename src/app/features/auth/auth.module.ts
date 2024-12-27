d7c7a7543b8cbfa501e3e6e49f23cca358a0f714
bisho515 <bishogeorge689@gmail.com>
2024-12-27 17:49:40 +0200

    [feat] add request-reset-component

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { RequestResetPasswordComponent } from './components/request-reset-password/request-reset-password.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RequestResetPasswordComponent],
   imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
---
+++ src/app/features/auth/components/request-reset-password/request-reset-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-reset-password',
+  templateUrl: './request-reset-password.component.html',
+  styleUrl: './request-reset-password.component.scss'
+})
+export class RequestResetPasswordComponent {
+
+}
