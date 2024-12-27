7fa35da411e8af923ed0346364408bc5757a99fb
bisho515 <bishogeorge689@gmail.com>
2024-12-27 18:12:33 +0200

    [feat] auth.module: add request-reset component to declerations

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { RequestResetPasswordComponent } from './components/request-reset-password/request-reset-password.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, RequestResetPasswordComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
