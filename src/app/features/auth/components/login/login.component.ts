4075350e74bcff77dc835a90e194f9487dd82df3
mahamfathy <mahafathyh@gmail.com>
2024-12-27 17:48:36 +0200

    edits

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  resMsg: string = '';
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
+  constructor(
+    private _AuthService: AuthService,
+    private _ToastrService: ToastrService
+  ) {}
+  onSubmit(data: FormGroup) {
+    const formValue = data.value;
+    if (data.valid) {
+      this._AuthService.onLogin(formValue).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.resMsg = res.message;
+          localStorage.setItem('token', res.data.token);
+          localStorage.setItem('role', res.data.user.role);
+        },
+        error: (err) => {
+          console.log(err);
+          this._ToastrService.error(err.message, 'Error');
+        },
+        complete: () => {
+          this._ToastrService.success(this.resMsg, 'Success');
+        },
+      });
+    }
+  }
 }
--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -7,7 +7,17 @@ import { ILogin, LoginResponse } from '../interfaces/ILogin';
   providedIn: 'root',
 })
 export class AuthService {
+  role: string | null = '';
   constructor(private http: HttpClient) {}
+  getRole(): string | null {
+    if (
+      localStorage.getItem('role') !== null &&
+      localStorage.getItem('token') !== null
+    ) {
+      this.role = localStorage.getItem('role');
+    }
+    return this.role;
+  }
   onLogin(data: ILogin): Observable<LoginResponse> {
     return this.http.post<LoginResponse>('admin/users/login', data);
   }
