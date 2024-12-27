ea660dac8a5e8660e1b1b3a3cc05e634f605227e
mahamfathy <mahafathyh@gmail.com>
2024-12-27 23:50:18 +0200

    [feat] sidebar component: finish sidebar

--- src/app/features/admin/dashboard/components/side-navbar/side-navbar.component.ts
+++ src/app/features/admin/dashboard/components/side-navbar/side-navbar.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
+interface IMenu {
+  name: string;
+  icon: string;
+  route: string;
+}
 @Component({
   selector: 'app-side-navbar',
   templateUrl: './side-navbar.component.html',
-  styleUrl: './side-navbar.component.scss'
+  styleUrl: './side-navbar.component.scss',
 })
 export class SideNavbarComponent {
-
+  isExpanded: boolean = true;
+  @Output() toggle: EventEmitter<boolean> = new EventEmitter<boolean>();
+  navItems: IMenu[] = [
+    {
+      name: 'Home',
+      icon: 'home',
+      route: '/admin/dashboard/home',
+    },
+    {
+      name: 'Users',
+      icon: 'people',
+      route: '/admin/dashboard/users',
+    },
+    {
+      name: 'Rooms',
+      icon: 'task',
+      route: '/admin/dashboard/rooms',
+    },
+    {
+      name: 'Ads',
+      icon: 'work',
+      route: '/dashboard/employee/ads',
+    },
+    {
+      route: '/booking',
+      name: 'Booking facilities',
+      icon: 'lock_open',
+    },
+    {
+      name: 'Logout',
+      icon: 'logout',
+      route: '/auth',
+    },
+  ];
+  toggleSidebar(): void {
+    this.isExpanded = !this.isExpanded;
+    this.toggle.emit();
+  }
+  logout(): void {}
 }
--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-
+  isExpand: boolean = true;
+  toggleSidebar(): void {
+    this.isExpand = !this.isExpand;
+  }
 }
