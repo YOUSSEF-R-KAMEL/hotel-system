46977a4c5d0a4017f4f9fd11555134b1c488142f
mahamfathy <mahafathyh@gmail.com>
2024-12-27 19:16:05 +0200

    [feat] header component: create header

--- src/app/features/dashboard/admin/admin.module.ts
+++ src/app/features/dashboard/admin/admin.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../../shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
-
+import { HeaderComponent } from './dashboard/components/header/header.component';
+import { HomeComponent } from './dashboard/components/home/home.component';
+import { SideNavbarComponent } from './dashboard/components/side-navbar/side-navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    DashboardComponent,
+    HeaderComponent,
+    SideNavbarComponent,
+    HomeComponent,
   ],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  imports: [CommonModule, AdminRoutingModule, SharedModule],
 })
-export class AdminModule { }
+export class AdminModule {}
