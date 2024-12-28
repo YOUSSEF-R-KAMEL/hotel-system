8bdb7fe38c288e4079c2bf742f1900066e1209ca
mahamfathy <mahafathyh@gmail.com>
2024-12-28 21:30:30 +0200

    [feat] ads interfaces: create Iads interface nd Iupdate

--- src/app/features/admin/dashboard/modules/ads/services/ads.service.ts
+++ src/app/features/admin/dashboard/modules/ads/services/ads.service.ts
@@ -1,13 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IAdsResponse } from '../interfaces/IAdsResponse';
+import { IUpdateResponse } from '../interfaces/IUpdateResponse.ts';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdsService {
   constructor(private _HttpClient: HttpClient) {}
-  onGetAllAds(): Observable<any> {
-    return this._HttpClient.get('admin/ads');
+  onGetAllAds(): Observable<IAdsResponse> {
+    return this._HttpClient.get<IAdsResponse>('admin/ads');
+  }
+
+  onUpdateAds(data: any): Observable<IUpdateResponse> {
+    return this._HttpClient.put<IUpdateResponse>('admin/ads', { data });
   }
 }
