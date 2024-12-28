388afdfcfa5a0ced2cc758d71702395ed58dda3f
mahamfathy <mahafathyh@gmail.com>
2024-12-28 02:45:09 +0200

    [feat] interceptor iuserresponse :  create interceptor iuserresponse

--- src/app/shared/services/helper/helper.service.ts
+++ src/app/shared/services/helper/helper.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUserResponse } from '../../interfaces/IUserResponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HelperService {
-
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) {}
+  onGetUser(id: number): Observable<IUserResponse> {
+    return this._HttpClient.get<IUserResponse>(`admin/users/${id}`);
+  }
 }
