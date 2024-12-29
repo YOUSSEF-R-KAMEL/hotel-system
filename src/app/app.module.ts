b2f088bb092a1e50960cd56db135b6e8ddbf8b89
mahamfathy <mahafathyh@gmail.com>
2024-12-29 17:44:09 +0200

    [feat] dashboard: edits in charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,11 @@ import {
 } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { globalInterceptor } from './core/interceptors/global/global.interceptor';
-import { NgxSpinner, NgxSpinnerModule } from 'ngx-spinner';
 import { loadingInterceptor } from './core/interceptors/loading/loading.interceptor';
 
 @NgModule({
@@ -33,8 +32,10 @@ import { loadingInterceptor } from './core/interceptors/loading/loading.intercep
   providers: [
     provideClientHydration(),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch(), withInterceptors([globalInterceptor, loadingInterceptor])),
-    [provideCharts(withDefaultRegisterables())],
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([globalInterceptor, loadingInterceptor])
+    ),
   ],
   bootstrap: [AppComponent],
 })
