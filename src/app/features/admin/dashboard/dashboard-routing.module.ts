8b78cd76d5e2096fd0f5ab7a5040a830729d63d3
mahamfathy <mahafathyh@gmail.com>
2024-12-31 21:30:24 +0200

    [feat] profile component : cerate profile component

--- src/app/features/admin/dashboard/dashboard-routing.module.ts
+++ src/app/features/admin/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
@@ -12,8 +13,16 @@ const routes: Routes = [
         path: 'home',
         component: HomeComponent,
       },
-      {path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
-      { path: 'rooms', loadChildren: () => import('./rooms/rooms.module').then(m => m.RoomsModule) },
+      {
+        path: 'users',
+        loadChildren: () =>
+          import('./users/users.module').then((m) => m.UsersModule),
+      },
+      {
+        path: 'rooms',
+        loadChildren: () =>
+          import('./rooms/rooms.module').then((m) => m.RoomsModule),
+      },
       {
         path: 'users',
         loadChildren: () =>
@@ -22,10 +31,9 @@ const routes: Routes = [
 
       {
         path: 'ads',
-        loadChildren: () =>
-          import('./ads/ads.module').then((m) => m.AdsModule),
+        loadChildren: () => import('./ads/ads.module').then((m) => m.AdsModule),
       },
-
+      { path: 'profile/:id', component: ProfileComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
     ],
   },
