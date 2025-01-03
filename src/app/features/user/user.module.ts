52d30b4a58097848697e8325e8e9dbc3abeafbd5
mahamfathy <mahafathyh@gmail.com>
2025-01-03 11:50:54 +0200

    [feat] shared-rooms component : create shared-rooms component

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { SharedRoomsComponent } from './shared-rooms/shared-rooms.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    SharedRoomsComponent
   ],
   imports: [
     CommonModule,
