f68dff88b3c2dbed06010fad3998f761113c23e5
mahamfathy <mahafathyh@gmail.com>
2025-01-03 23:57:54 +0200

    edits

--- src/app/features/user/user-routing.module.ts
+++ src/app/features/user/user-routing.module.ts
@@ -3,14 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 
 const routes: Routes = [
-  { path: '', component: UserComponent },
-  {path: 'ads-list', loadChildren: () => import('./ads-list/ads-list.module').then(m => m.AdsListModule)}
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: UserComponent }
+  {
+    path: '',
+    component: UserComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
+      },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
