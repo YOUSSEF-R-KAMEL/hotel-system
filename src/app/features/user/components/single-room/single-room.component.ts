bc9d849863f1ace1c1347e8873c60c7b14cc4ad1
mahamfathy <mahafathyh@gmail.com>
2025-01-05 21:01:26 +0200

    [feat] explore component : finish styling and ts

--- src/app/features/user/components/single-room/single-room.component.ts
+++ src/app/features/user/components/single-room/single-room.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IRoom } from '../../../../shared/interface/room/room.interface';
 
 @Component({
   selector: 'app-single-room',
   templateUrl: './single-room.component.html',
-  styleUrl: './single-room.component.scss'
+  styleUrl: './single-room.component.scss',
 })
 export class SingleRoomComponent {
-
+  @Input() room!: IRoom;
 }
--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../../shared/shared.module';
 import { ExploreComponent } from './components/explore/explore.component';
+import { SingleRoomComponent } from './components/single-room/single-room.component';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 
 @NgModule({
-  declarations: [UserComponent, ExploreComponent],
+  declarations: [UserComponent, ExploreComponent, SingleRoomComponent],
   imports: [CommonModule, UserRoutingModule, SharedModule],
+  exports: [SingleRoomComponent],
 })
 export class UserModule {}
