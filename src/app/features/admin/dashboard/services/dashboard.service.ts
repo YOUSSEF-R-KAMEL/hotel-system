3c38e11a8567e58eb0472222cfab236ec726ce1d
mahamfathy <mahafathyh@gmail.com>
2025-01-05 18:54:43 +0200

    [feat] explore component : create component

--- src/app/features/admin/dashboard/services/dashboard.service.ts
+++ src/app/features/admin/dashboard/services/dashboard.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 import { IChangePassword } from '../Interfaces/ichange-password';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DashboardService {
-  constructor(private _http:HttpClient) { }
-  changePassword(data:IChangePassword){
-    return this._http.post("admin/users//change-password", data)
+  constructor(private _http: HttpClient) {}
+  changePassword(data: IChangePassword) {
+    return this._http.post('admin/users/change-password', data);
   }
 }
--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../../shared/shared.module';
+import { ExploreComponent } from './components/explore/explore.component';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, ExploreComponent],
   imports: [CommonModule, UserRoutingModule, SharedModule],
 })
 export class UserModule {}
