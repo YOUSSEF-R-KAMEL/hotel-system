ca15f8d8f30a85443d7f3da19e41e7d432f95354
mahamfathy <mahafathyh@gmail.com>
2025-01-05 21:02:33 +0200

    [chore] pagination : install ngx-paginator

--- src/app/features/user/home/components/shared-rooms/shared-rooms.component.ts
+++ src/app/features/user/home/components/shared-rooms/shared-rooms.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IRoom } from '../../../../../shared/interface/room/room.interface';
+import { RoomsService } from '../../../services/rooms.service';
 
 @Component({
   selector: 'app-shared-rooms',
   templateUrl: './shared-rooms.component.html',
-  styleUrl: './shared-rooms.component.scss'
+  styleUrl: './shared-rooms.component.scss',
 })
-export class SharedRoomsComponent {
-
+export class SharedRoomsComponent implements OnInit {
+  rooms: IRoom[] = [];
+  page: number = 1;
+  size: number = 10;
+  constructor(private _RoomsService: RoomsService) {}
+  ngOnInit(): void {
+    this.getAllRooms();
+  }
+  getAllRooms() {
+    this._RoomsService
+      .getAllRooms({ page: this.page, size: this.size })
+      .subscribe({
+        next: (res) => {
+          this.rooms = res.data.rooms as IRoom[];
+          console.log(this.rooms);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
 }
