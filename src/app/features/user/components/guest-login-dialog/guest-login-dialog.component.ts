035b24c73522fdd9eaf306155815fe54dc8b8b47
mahamfathy <mahafathyh@gmail.com>
2025-01-07 23:49:37 +0200

    [feat] guest-login-dialog component : create dialog

---
+++ src/app/features/user/components/guest-login-dialog/guest-login-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-login-dialog',
+  templateUrl: './guest-login-dialog.component.html',
+  styleUrl: './guest-login-dialog.component.scss'
+})
+export class GuestLoginDialogComponent {
+
+}
--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -6,9 +6,10 @@ import { ExploreComponent } from './components/explore/explore.component';
 import { RoomDetailsComponent } from './home/components/room-details/room-details.component';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { GuestLoginDialogComponent } from './components/guest-login-dialog/guest-login-dialog.component';
 
 @NgModule({
-  declarations: [UserComponent, ExploreComponent, RoomDetailsComponent],
+  declarations: [UserComponent, ExploreComponent, RoomDetailsComponent, GuestLoginDialogComponent],
   imports: [CommonModule, UserRoutingModule, SharedModule, NgxPaginationModule],
 })
 export class UserModule {}
