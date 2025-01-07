0a1eb12137919d025d3781c304d4d5b497588a22
mahamfathy <mahafathyh@gmail.com>
2025-01-08 00:57:13 +0200

    [feat]login-register-dialog component : create dialog

--- src/app/features/user/components/single-room/single-room.component.ts
+++ src/app/features/user/components/single-room/single-room.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { IRoom } from '../../../../shared/interface/room/room.interface';
+import { AuthService } from '../../../auth/services/auth.service';
+import { LoginRegisterDialogComponent } from '../login-register-dialog/login-register-dialog.component';
 
 @Component({
   selector: 'app-single-room',
   templateUrl: './single-room.component.html',
   styleUrl: './single-room.component.scss',
 })
-export class SingleRoomComponent implements OnInit {
+export class SingleRoomComponent {
   @Input() room: IRoom | null = null;
-  constructor() {
-  }
-  ngOnInit(): void {
+  constructor(
+    public dialog: MatDialog,
+    private _AuthService: AuthService,
+    private _Router: Router
+  ) {}
+  openDialog(): void {
+    if (
+      this._AuthService.role() !== 'user' &&
+      this._AuthService.role() !== 'admin'
+    ) {
+      const dialogRef = this.dialog.open(LoginRegisterDialogComponent);
+    }
   }
 }
--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { SharedModule } from '../../shared/shared.module';
 import { ExploreComponent } from './components/explore/explore.component';
+import { LoginRegisterDialogComponent } from './components/login-register-dialog/login-register-dialog.component';
 import { RoomDetailsComponent } from './home/components/room-details/room-details.component';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
-import { GuestLoginDialogComponent } from './components/guest-login-dialog/guest-login-dialog.component';
 
 @NgModule({
-  declarations: [UserComponent, ExploreComponent, RoomDetailsComponent, GuestLoginDialogComponent],
+  declarations: [
+    UserComponent,
+    ExploreComponent,
+    RoomDetailsComponent,
+    LoginRegisterDialogComponent,
+  ],
   imports: [CommonModule, UserRoutingModule, SharedModule, NgxPaginationModule],
 })
 export class UserModule {}
