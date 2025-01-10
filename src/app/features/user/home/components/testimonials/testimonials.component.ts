738476de2f2407172f7d9990fbe8f3ee1f4eae01
bisho515 <bishogeorge689@gmail.com>
2025-01-11 00:13:25 +0200

    [feat] finish testimonial api integration

--- src/app/features/user/home/components/testimonials/testimonials.component.ts
+++ src/app/features/user/home/components/testimonials/testimonials.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RoomsService } from '../../../services/rooms.service';
+import { IApiResponse } from '../../../../../shared/interface/api-data-response/api-response.interface';
+import { IRoom } from '../../../../../shared/interface/room/room.interface';
+import { IGetRoomReview } from '../../../interfaces/room-review.interface';
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
   styleUrl: './testimonials.component.scss'
 })
-export class TestimonialsComponent {
-  stars: number[] = Array(5).fill(0);  // Creates an array of 5 elements
-
+export class TestimonialsComponent implements OnInit{
+  stars: number[] = Array(5).fill(0);
+  @Input() room: IRoom | null = null;
+  reviews: IGetRoomReview[] = [];
+  constructor(private roomsService: RoomsService) {}
+  ngOnInit(): void {
+    if (this.room) {
+      this.roomsService.getRoomReviews(this.room?._id as string).subscribe({
+        next: (res: IApiResponse) => {
+          this.reviews = res.data.roomReviews as IGetRoomReview[];
+          console.log(this.reviews)
+        }
+      })
+    }
+  }
 }
--- src/app/features/user/home/home.module.ts
+++ src/app/features/user/home/home.module.ts
@@ -7,9 +7,10 @@ import { SharedRoomsComponent } from './components/shared-rooms/shared-rooms.com
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { TestimonialCarouselComponent } from './components/testimonial-carousel/testimonial-carousel.component';
 
 @NgModule({
-  declarations: [HomeComponent, SharedRoomsComponent, PopularRoomsComponent, TestimonialsComponent],
+  declarations: [HomeComponent, SharedRoomsComponent, PopularRoomsComponent, TestimonialsComponent, TestimonialCarouselComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
